649e36d72c0fa4c276f653ae45f44911d99e9864
Leah Ramsier <leahramsier@gmail.com>
2022-10-21 15:05:24 -0400

    testing cbbpolldotnet

--- pages/profile.js
+++ pages/profile.js
@@ -40,7 +40,7 @@ export default function Profile({ user }) {
 }
 
 //const REDIRECT_URI = "http://localhost:3000/profile";
-const REDIRECT_URI = "http://cbbpolldotnet.vercel.app/profile";
+const REDIRECT_URI = "http://cbbpoll.net/profile";
 const RANDOM_STRING = "randomstringhere";
 const CLIENT_ID = process.env.REDDIT_CLIENT_ID;
 const CLIENT_SECRET = process.env.REDDIT_CLIENT_SECRET;
