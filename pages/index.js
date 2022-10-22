e3535e4e3c68703edb77ad790ce03c4963e9f263
thefaultinourcode <leahramsier@gmail.com>
2022-10-22 17:12:20 -0400

    ???

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ import Image from 'next/image';
 const DURATION = "permanent";
 const SCOPE = "identity";
 
-const REDIRECT_URI = process.env.REDIRECT_URI;
+//const REDIRECT_URI = process.env.REDIRECT_URI;
+const REDIRECT_URI = "http://cbbpoll.net/profile";
 
 const RANDOM_STRING = "randomstringhere"; //randomstring.generate();
 const RESPONSE_TYPE = "code";
