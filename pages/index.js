514564573f65af698005614ab76e78a57480f87d
thefaultinourcode <leahramsier@gmail.com>
2022-10-22 17:06:11 -0400

    ahhhhh

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,7 @@ import Image from 'next/image';
 const DURATION = "permanent";
 const SCOPE = "identity";
 
-const REDIRECT_URI = "http://localhost:3000/profile";
-//const REDIRECT_URI = "http://cbbpoll.net/profile";
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 const RANDOM_STRING = "randomstringhere"; //randomstring.generate();
 const RESPONSE_TYPE = "code";
