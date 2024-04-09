e5fe2897a80cbf4cbafc1706853d9f08646eab75
thefaultinourcode <leahramsier@gmail.com>
2024-04-08 23:39:10 -0500

    post-season

--- pages/seasons/[season]/[week].js
+++ pages/seasons/[season]/[week].js
@@ -44,7 +44,7 @@ export default function Week(props) {
 	}
 
 	console.log('week.week:', week.week);
-	if (week.week === '9' && weekNum === 8) {
+	if (week.week === 'Post-Season' && weekNum === 20) {
 		return (
 			<div>
 				<p>You do not have permission to view this page.</p>
