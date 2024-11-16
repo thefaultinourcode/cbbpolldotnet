ca745265c33b568189abaefaea19bb16e8f62f0a
thefaultinourcode <leahramsier@gmail.com>
2024-11-16 00:05:25 -0600

    week 3

--- utils/getDates.js
+++ utils/getDates.js
@@ -12,12 +12,12 @@
 
 export const getWeek = () => {
 
-    let week = 2;
+    let week = 3;
     return week;
 }
 
 export const getPriorWeek = () => {
-    let week = 'Pre-Season';
+    let week = 2;
     
     return week;
 }
@@ -26,13 +26,13 @@ export const getPriorWeek = () => {
 export const getPollDate = () => {};
 
 export const getOpenDate = () => {
-    let date = new Date('9 November 2024 15:00 UTC');
+    let date = new Date('16 November 2024 15:00 UTC');
 
 	return date;
 };
 
 export const getCloseDate = () => {
-    let date = new Date('11 November 2024 15:00 UTC');
+    let date = new Date('18 November 2024 15:00 UTC');
 
 	return date;
 };
