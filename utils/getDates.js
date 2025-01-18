3ea0224ab4922c9459b4c9d0ca348d3a13c46217
thefaultinourcode <leahramsier@gmail.com>
2025-01-17 18:00:24 -0600

    week 12

--- utils/getDates.js
+++ utils/getDates.js
@@ -12,12 +12,12 @@
 
 export const getWeek = () => {
 
-    let week = 11;
+    let week = 12;
     return week;
 }
 
 export const getPriorWeek = () => {
-    let week = 10;
+    let week = 11;
     
     return week;
 }
@@ -26,13 +26,13 @@ export const getPriorWeek = () => {
 export const getPollDate = () => {};
 
 export const getOpenDate = () => {
-    let date = new Date('11 January 2025 15:00 UTC');
+    let date = new Date('18 January 2025 15:00 UTC');
 
 	return date;
 };
 
 export const getCloseDate = () => {
-    let date = new Date('13 January 2025 15:00 UTC');
+    let date = new Date('20 January 2025 15:00 UTC');
 
 	return date;
 };
