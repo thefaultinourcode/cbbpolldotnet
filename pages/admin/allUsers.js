14bf36d0fe1d917a143b41ee6832f7bec7c54793
thefaultinourcode <leahramsier@gmail.com>
2025-11-10 23:53:54 -0600

    season 2026

--- pages/admin/allUsers.js
+++ pages/admin/allUsers.js
@@ -213,8 +213,8 @@ export default function Admin(props){
 
 }
 
-const REDIRECT_URI = "http://localhost:3000/profile";
-//const REDIRECT_URI = "http://cbbpoll.net/profile";
+//const REDIRECT_URI = "http://localhost:3000/profile";
+const REDIRECT_URI = "http://cbbpoll.net/profile";
 
 const RANDOM_STRING = "randomstringhere";
 const CLIENT_ID = process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID;
